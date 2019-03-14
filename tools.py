c6d00ca6bbfaa6cd9db7b04e8c374086dc9aa8e5
xiaozh <1298856981@qq.com>
2019-03-14 19:39:09 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -8,25 +8,6 @@ from scipy.stats.stats import pearsonr
 from glob import glob
 from subprocess import Popen
 
-class Logger(object):
-    def __init__(self,file):
-        self.terminal = sys.stdout
-        self.log = open(file, 'wt')
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.terminal.flush()
-        self.log.write(message)  
-        self.log.flush()        
-    
-    def flush(self):
-        #this flush method is needed for python 3 compatibility.
-        #this handles the flush command by doing nothing.
-        #you might want to specify some extra behavior here.
-        self.log.close()
-        sys.stdout=self.terminal
-        pass 
-
 def SaveMkdir(dir):
     try:
         if not os.path.exists(dir):
